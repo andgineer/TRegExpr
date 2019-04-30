a51e1b59afea2c8e341a3dd37ab057de38c49319
Andrei Sorokin2 <Andrei_Sorokin2@epam.com>
2019-04-30 17:59:36 +0300

    Draft 2 Russian doc translation

--- docs/build.sh
+++ docs/build.sh
@@ -3,9 +3,9 @@
 # Build translated docs in _build/html/ru_RU
 #
 sphinx-build -b html -D language=ru_RU . _build/html/ru
-sphinx-build -b html -D language=es_ES . _build/html/es
-sphinx-build -b html -D language=bg_BG . _build/html/bg
-sphinx-build -b html -D language=de_DE . _build/html/de
-sphinx-build -b html -D language=fr_FR . _build/html/fr
+#sphinx-build -b html -D language=es_ES . _build/html/es
+#sphinx-build -b html -D language=bg_BG . _build/html/bg
+#sphinx-build -b html -D language=de_DE . _build/html/de
+#sphinx-build -b html -D language=fr_FR . _build/html/fr
 google-chrome _build/html/ru/index.html
 
