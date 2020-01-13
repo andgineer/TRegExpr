3d292fcd57db2978cc777ec393ad2a98dfc35a9d
Alexey-t <support@uvviewsoft.com>
2020-01-13 13:03:08 +0300

    add CI testing for 2 new buildmodes

--- test.sh
+++ test.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
-echo To install free pascal run ./.travis-lazarus/.travis.install.py
+echo To install Free Pascal, run ./.travis-lazarus/.travis.install.py
 
 rm -r test/bin
+
 if lazbuild test/test_fpc.lpi ; then
+    test/test_fpc --all --format=plain
+    else echo "*** Compilation error ***"
+  fi
+
+if lazbuild -r --bm=Unicode test/test_fpc.lpi ; then
+    test/test_fpc --all --format=plain
+    else echo "*** Compilation error ***"
+  fi
+
+if lazbuild -r --bm=UnicodeAndChars test/test_fpc.lpi ; then
     test/test_fpc --all --format=plain
     else echo "*** Compilation error ***"
   fi
