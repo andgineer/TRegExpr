fdfbe5200f2bfc553278597bd3bd849452f49a48
Alexey <support@uvviewsoft.com>
2020-08-30 01:02:33 +0300

    history

--- src/regexpr.pas
+++ src/regexpr.pas
@@ -814,7 +814,7 @@ uses
 const
   // TRegExpr.VersionMajor/Minor return values of these constants:
   REVersionMajor = 1;
-  REVersionMinor = 117;
+  REVersionMinor = 119;
 
   OpKind_End = REChar(1);
   OpKind_MetaClass = REChar(2);
