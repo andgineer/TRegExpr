e315e2595eb2ee4cbae50630d68f84ddb4832604
Alexey <support@uvviewsoft.com>
2022-05-06 00:23:55 +0300

    move new Ifdef little lower

--- src/regexpr.pas
+++ src/regexpr.pas
@@ -76,16 +76,15 @@ interface
 {$ENDIF}
 // ======== Define options for TRegExpr engine
 {$DEFINE UnicodeRE} // Use WideChar for characters and UnicodeString/WideString for strings
+{ off $DEFINE UnicodeEx} // Support Unicode >0xFFFF, e.g. emoji, e.g. "." must find 2 WideChars of 1 emoji
+{ off $DEFINE UseWordChars} // Use WordChars property, otherwise fixed list 'a'..'z','A'..'Z','0'..'9','_' 
+{ off $DEFINE UseSpaceChars} // Use SpaceChars property, otherwise fixed list
+{ off $DEFINE UseLineSep} // Use LineSeparators property, otherwise fixed line-break chars
 {$IFDEF UNICODE}
   {$IFNDEF UnicodeRE}
   {$MESSAGE ERROR 'You cannot undefine UnicodeRE for Unicode Delphi versions'}
   {$ENDIF}
 {$ENDIF}
-
-{ off $DEFINE UnicodeEx} // Support Unicode >0xFFFF, e.g. emoji, e.g. "." must find 2 WideChars of 1 emoji
-{ off $DEFINE UseWordChars} // Use WordChars property, otherwise fixed list 'a'..'z','A'..'Z','0'..'9','_' 
-{ off $DEFINE UseSpaceChars} // Use SpaceChars property, otherwise fixed list
-{ off $DEFINE UseLineSep} // Use LineSeparators property, otherwise fixed line-break chars
 {$IFDEF FPC}
   {$DEFINE FastUnicodeData} // Use arrays for UpperCase/LowerCase/IsWordChar, they take 320K more memory
 {$ENDIF}
