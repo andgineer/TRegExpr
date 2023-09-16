06b96b20864e16a4ba4a957eedf5a5e6b8b5dfe2
Martin <mfr-git@mfriebe.de>
2023-09-16 13:13:29 +0200

    History

--- src/regexpr.pas
+++ src/regexpr.pas
@@ -903,7 +903,7 @@ uses
 const
   // TRegExpr.VersionMajor/Minor return values of these constants:
   REVersionMajor = 1;
-  REVersionMinor = 167;
+  REVersionMinor = 169;
 
   OpKind_End = REChar(1);
   OpKind_MetaClass = REChar(2);
