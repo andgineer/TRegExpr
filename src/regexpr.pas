18ad26b2bee562c00b8800ea4a20df1f946933e0
Alexey <support@uvviewsoft.com>
2023-11-29 09:42:59 +0300

    version

--- src/regexpr.pas
+++ src/regexpr.pas
@@ -933,7 +933,7 @@ uses
 const
   // TRegExpr.VersionMajor/Minor return values of these constants:
   REVersionMajor = 1;
-  REVersionMinor = 181;
+  REVersionMinor = 182;
 
   OpKind_End = REChar(1);
   OpKind_MetaClass = REChar(2);
