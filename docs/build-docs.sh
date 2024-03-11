129b1cddcc817551f3fcf1164fd2a8ad1bdc1f86
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-03-11 19:19:36 +0100

    fixed en-link level

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -11,8 +11,11 @@ for lang in en bg de es fr ru; do  # en should be the first language as it clear
     if [ $lang = "en" ]; then
       mkdir -p ../site/en
       cp -r ../site/latest ../site/en/latest
+      python fix_folder_redirects.py ../site/$lang/latest --shift-num=2
+    else
+      python fix_folder_redirects.py ../site/$lang/latest
     fi
-    python fix_folder_redirects.py ../site/$lang/latest
+
     rm _mkdocs.yml
     rm -rf $lang/_css
 done
--- docs/fix_folder_redirects.py
+++ docs/fix_folder_redirects.py
@@ -3,20 +3,21 @@ import shutil
 import argparse
 
 
-def shift_path_one_level_up(file_path):
+def shift_path_up(file_path, shift_num):
     with open(file_path, 'r', encoding='utf-8') as file:
         file_content = file.read()
 
-    # Replace the old string with the new string
-    updated_content = file_content.replace("../../", "../")
+    old_string = "../" * (shift_num + 1)
+    new_string = "../" * shift_num
 
-    # Write the updated content back to the file
+    updated_content = file_content.replace(old_string, new_string)
+
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(updated_content)
 
 
-def fix_redirects(site_dir):
-    print(f"Fixing folder redirects in {site_dir}")
+def fix_redirects(site_dir, shift_num):
+    print(f"Fixing folder redirects in {site_dir}, shifting paths by {shift_num} level(s).")
     for root, dirs, files in os.walk(site_dir):
         for dir_name in dirs:
             source_path = os.path.join(root, dir_name, "index.html")
@@ -27,7 +28,7 @@ def fix_redirects(site_dir):
 
                 # Copy <folder>/index.html to <folder>.html
                 shutil.copy2(source_path, dest_path)
-                shift_path_one_level_up(dest_path)
+                shift_path_up(dest_path, shift_num)
                 print(f"Copied: {source_path} to {dest_path}")
             else:
                 print(f"index.html not found for {source_path}")
@@ -36,6 +37,7 @@ def fix_redirects(site_dir):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert all MkDocs folder redirects to HTML equivalents.")
     parser.add_argument("site_dir", help="The path to the MkDocs site directory to be processed.")
+    parser.add_argument("--shift-num", type=int, default=1, help="The number of '../' levels to remove from paths.")
 
     args = parser.parse_args()
-    fix_redirects(args.site_dir)
+    fix_redirects(args.site_dir, args.shift_num)
