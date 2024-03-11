6a1732828f848c2494488c2b7d9538c5aa5f0483
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-03-11 19:27:05 +0100

    found yet another old readthedocs URL variation

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -11,9 +11,12 @@ for lang in en bg de es fr ru; do  # en should be the first language as it clear
     if [ $lang = "en" ]; then
       mkdir -p ../site/en
       cp -r ../site/latest ../site/en/latest
+      cp -r ../site/stable ../site/en/stable
       python fix_folder_redirects.py ../site/$lang/latest --shift-num=2
+      python fix_folder_redirects.py ../site/$lang/stable --shift-num=2
     else
       python fix_folder_redirects.py ../site/$lang/latest
+      python fix_folder_redirects.py ../site/$lang/stable
     fi
 
     rm _mkdocs.yml
